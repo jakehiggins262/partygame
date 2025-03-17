fd19bf12e32fbf3f7ebc5c6d9cbc6f11ac2e4998
jakehiggins262 <jakehiggins262@gmail.com>
2025-03-17 18:46:39 -0400

    Added controller move

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -1,45 +1,51 @@
 extends CharacterBody2D
 
-@export var move_speed: float = 100  #@export allows us to edit this value in the inspector on the right
+@export var move_speed: float = 100  # @export allows us to edit this value in the inspector on the right
 @export var starting_direction: Vector2 = Vector2(0, 1)
 
-
-#parameters/Idle/blend_position
-#when script starts we get access to this node that is in our scene
-#this will look at our scene, find the object named AnimationTree, store in variable
 @onready var animation_tree = $AnimationTree
-
-#get the playback componenet from animation tree (AnimationNodeStateMachinePlayback)
 @onready var state_machine = animation_tree.get("parameters/playback")
+
+# Variable to store the last direction
+var last_direction: Vector2 = Vector2.ZERO
 
 func _ready():
 	update_animation_parameters(starting_direction)
 
-
 func _physics_process(_delta):
-	#Get Input Directions
+	# Get Input Directions
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
+	# Update the last direction if there's input
+	if input_direction.length() > 0:
+		last_direction = input_direction.normalized()
+
 	update_animation_parameters(input_direction)
-	#Update velocity
+	
+	# Update velocity
 	velocity = input_direction * move_speed
-	#Move and Slide function uses velocity or character body to move character on map
+	
+	# Rotate to the last valid direction if idle
+	if velocity == Vector2.ZERO and last_direction != Vector2.ZERO:
+		rotation = last_direction.angle()
+	else:
+		rotation = velocity.angle()
+	
 	move_and_slide()
 	pick_new_state()
-	
-	
+
 func update_animation_parameters(move_input: Vector2):
-	
-	#Don't change animation aparameters if there is no move input
-	if(move_input != Vector2.ZERO):
+	# Don't change animation parameters if there is no move input
+	if move_input != Vector2.ZERO:
 		animation_tree.set("parameters/Walk/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
-		
+
 func pick_new_state():
-	if(velocity != Vector2.ZERO):
+	# Transition between walk and idle states based on velocity
+	if velocity != Vector2.ZERO:
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
