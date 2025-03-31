9c29c7ee3c8cefdd47ada30895cdbb14c2a7f110
jakehiggins262 <jakehiggins262@gmail.com>
2025-03-31 12:18:19 -0400

    Added respawn timer to gunpickup

--- Scripts/gun_pickup.gd
+++ Scripts/gun_pickup.gd
@@ -1,11 +1,24 @@
-extends Area2D
+extends Node2D
 
 @export var gun_scene: PackedScene
+@export var respawn_time: float
+@onready var sprite = $Sprite2D
+@onready var collision_shape = $Area2D/CollisionShape2D
+
+var is_available = true
 
 func _ready():
-	connect("body_entered", Callable(self, "_on_body_entered"))
+	sprite.texture = preload("res://Art/Jake/gunpickupavail.png")
 
-func _on_body_entered(body):
-	if body is CharacterBody2D and body.has_method("equip_weapon"):
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if is_available and body is CharacterBody2D and body.has_method("equip_weapon"):
+		is_available = false
 		body.equip_weapon(gun_scene)
-		queue_free()
+		sprite.texture = preload("res://Art/Jake/gunpickup.png")
+		collision_shape.set_deferred("disabled", true)
+		$Timer.start(respawn_time) # 2-second respawn timer
+
+func _on_timer_timeout() -> void:
+	is_available = true
+	sprite.texture = preload("res://Art/Jake/gunpickupavail.png")
+	collision_shape.set_deferred("disabled", false)
