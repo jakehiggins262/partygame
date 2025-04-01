b36f965f3d1dc2cdeccc530df092701f41aac42e
jakehiggins262 <jakehiggins262@gmail.com>
2025-03-31 23:19:44 -0400

    added gun powerup and lms scene

---
+++ Scripts/sword_pickup.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+@export var sword_scene: PackedScene
+@export var respawn_time: float
+@onready var sprite = $Sprite2D
+@onready var collision_shape = $Area2D/CollisionShape2D
+
+var is_available = true
+
+func _ready():
+	sprite.texture = preload("res://Art/Jake/swordpickupavail.png")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if is_available and body is CharacterBody2D and body.has_method("equip_weapon"):
+		is_available = false
+		
+		# Instantiate the new weapon (sword)
+		var new_weapon = sword_scene.instantiate()
+		body.equip_weapon(sword_scene)
+		
+		#temp fix for hitbox being enabled when weapon is picked up
+		body.current_weapon.attack()
+		
+		# Update sprite and disable the collision shape for respawn
+		sprite.texture = preload("res://Art/Jake/swordpickup.png")
+		collision_shape.set_deferred("disabled", true)
+		$Timer.start(respawn_time)
+
+
+
+
+func _on_timer_timeout() -> void:
+	is_available = true
+	sprite.texture = preload("res://Art/Jake/swordpickupavail.png")
+	collision_shape.set_deferred("disabled", false)
